6097be420be716b8b0f458d79657e5531af78a54
jh <bis04199@gmail.com>
2022-12-19 12:37:22 +0900

    221219

--- src/test/java/com/team5/kyobo/KyoboApplicationTests.java
+++ src/test/java/com/team5/kyobo/KyoboApplicationTests.java
@@ -1,13 +1,23 @@
 package com.team5.kyobo;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.team5.kyobo.entity.BookDetailViewEntity;
+import com.team5.kyobo.repository.BookDetailViewRepository;
 
 @SpringBootTest
 class KyoboApplicationTests {
-
+	@Autowired BookDetailViewRepository bViewRepository;
 	@Test
-	void contextLoads() {
+	void 책리스트조회() {
+		List<BookDetailViewEntity> list = bViewRepository.findAll();
+		for(BookDetailViewEntity b : list){
+			System.out.println(b);
+		}
 	}
 
 }
